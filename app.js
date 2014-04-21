c74fef1c046ce4c95932dd6ae47566f3c9ef4b1b
Marius Mueller <mmuelle5@hsz-t.ch>
2014-04-21 23:01:45 +0200

    blubb

--- app.js
+++ app.js
@@ -7,18 +7,9 @@ var express = require('express'),
 
 var app = express();
 
-// all environments
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(responder);
-app.use(express.logger());
+app.use(logger);
 var server = http.createServer(app);
 
 
